01eaf5236252f3499bf70e6e8cfbc72bc951238e
lastreasure <43856727+lastreasure@users.noreply.github.com>
2022-05-11 23:09:19 +0100

    adding error handling to app

--- app.py
+++ app.py
@@ -263,6 +263,57 @@ def create_app(test_config=None):
             print(sys.exc_info())
             abort(500)
 
+    ### ERROR HANDLING ###
+    @app.errorhandler(404)
+    def unprocessable(err):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Resource Not Found'
+        }), 404
+
+    @app.errorhandler(400)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Client Error: Bad Request'
+        }), 400
+
+    @app.errorhandler(405)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': 'Method Not Allowed'
+        }), 405
+
+
+    @app.errorhandler(422)
+    def unproccessable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'Unproccessable'
+        }), 422
+
+
+    @app.errorhandler(500)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'internal server error'
+        }), 500
+
+    @app.errorhandler(AuthError)
+    def handle_auth_error(error):
+        return jsonify({
+            'success': False,
+            'error': error.status_code,
+            'message': error.error['description']
+        }), error.status_code
+
     return app
 
 app = create_app()
